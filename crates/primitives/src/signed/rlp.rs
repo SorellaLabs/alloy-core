24ee9c116e6a46471ab789a73c23dd53b20dacfd
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-10-22 10:04:48 -0700

    have fun

--- crates/primitives/src/signed/rlp.rs
+++ crates/primitives/src/signed/rlp.rs
@@ -1,8 +1,7 @@
-use crate::Signed;
+use crate::{Signed, Uint};
 use alloy_rlp::{
     length_of_length, BufMut, Decodable, DecodeError, Encodable, Header, EMPTY_STRING_CODE,
 };
-
 const MAX_BITS: usize = 55 * 8;
 
 impl<const BITS: usize, const LIMBS: usize> Encodable for Signed<BITS, LIMBS> {
@@ -20,16 +19,8 @@ impl<const BITS: usize, const LIMBS: usize> Encodable for Signed<BITS, LIMBS> {
 impl<const BITS: usize, const LIMBS: usize> Decodable for Signed<BITS, LIMBS> {
     #[inline]
     fn decode(buf: &mut &[u8]) -> Result<Self, DecodeError> {
-        //TODO: decode as unsigned
-        //convert to signed type
-    
-        let header = Header::decode(buf)?;
-        if header.list {
-            return Err(DecodeError::UnexpectedList)
-        }
-        let bytes = &buf[..header.payload_length];
-        *buf = &buf[header.payload_length..];
-        Self::try_from_be_slice(bytes).ok_or(DecodeError::Overflow)
+        let bytes = Uint::<BITS, LIMBS>::decode(buf)?;
+        Ok(Self::from_raw(bytes))
     }
 }
 
@@ -37,6 +28,29 @@ impl<const BITS: usize, const LIMBS: usize> Decodable for Signed<BITS, LIMBS> {
 mod tests {
     use super::*;
     use bytes::BytesMut;
+
+    use crate::{
+        aliases::{I0, I256, I64, I8, U0, U256, U64, U8},
+        const_for, nlimbs,
+    };
+    use hex_literal::hex;
+    use proptest::proptest;
+
+    fn encode<T: Encodable>(value: T) -> Vec<u8> {
+        let mut buf = vec![];
+        value.encode(&mut buf);
+        buf
+    }
+
+    #[test]
+    fn test_rlp() {
+        // See <https://github.com/paritytech/parity-common/blob/436cb0827f0e3238ccb80d7d453f756d126c0615/rlp/tests/tests.rs#L214>
+        assert_eq!(encode(U0::from(0))[..], hex!("80"));
+        assert_eq!(encode(U256::from(0))[..], hex!("80"));
+        assert_eq!(encode(U256::from(15))[..], hex!("0f"));
+        assert_eq!(encode(U256::from(1024))[..], hex!("820400"));
+        assert_eq!(encode(U256::from(0x1234_5678))[..], hex!("8412345678"));
+    }
 
     #[test]
     pub fn rlp_small() {
